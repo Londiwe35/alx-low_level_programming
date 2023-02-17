4519131895f74d960938e66125cf1894b86d0f09
Londiwe <londiwek1@utlook.com>
2023-02-16 18:00:28 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by a newline, to standard error.
- * Return 1 (Success)
+ * main - A C program that prints a newline to the standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
