418e59fe438562f1c8db75f903e2bd454a3553f0
Londiwe <londiwek1@utlook.com>
2023-02-17 14:58:17 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse, followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char lr;
+
+	for (lr = 'z'; lr <= 'a'; lr--)
+		putchar(lr);
+
+	putchar('\n')
+
+		return (0);
+
