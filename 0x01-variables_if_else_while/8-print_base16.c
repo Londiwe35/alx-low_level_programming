c8faa846fb4b0189242939d6df4c9bd4032da9f5
Londiwe <londiwek1@utlook.com>
2023-02-17 15:32:11 -0800

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,8 @@ int main(void)
 	int num;
 	char le;
 
-	for (num = '0'; num < '10'; num++)
-		putchar((num % 10) + '0');
+	for (num = '0'; num <= '9'; num++)
+		putchar((num);
 
 	for (le = 'a'; le <= 'f'; le++)
 		putchar(le);
