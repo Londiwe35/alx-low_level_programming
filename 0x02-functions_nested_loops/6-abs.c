e0f7110854e9c14b1fbff20d1e99616955c63a14
Londiwe <londiwek1@utlook.com>
2023-02-21 14:12:24 -0800

    interger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ * @c: the integer value to be computed
+ * Return: Absolute value of number 0r zero
+ */
+
+int _abs(int c)
+{
+
+	if (c < 0)
+	{
+	int abs_val;
+	
+	abs_val= c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
