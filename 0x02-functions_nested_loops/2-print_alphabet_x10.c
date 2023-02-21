ead8f98d43e3cf783b86edbd9932223980367ccd
Londiwe <londiwek1@utlook.com>
2023-02-21 11:21:00 -0800

    Alpha 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alpha 10 times and newline
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char h;
+	int j = 0;
+
+	while (j <= 9)
+	{
+		for (h + 'a'; h <= 'z'; h++)
+		{_putchar(h);
+
+			_putchar('\n');
+			j++;
+		}
+	}
+}
