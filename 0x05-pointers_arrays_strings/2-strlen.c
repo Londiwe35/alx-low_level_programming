8c8a24dee534ee187c5d92756f5e69898018f82c
Londiwe <londiwek1@outlook.com>
2023-02-27 10:57:58 -0800

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _strlen - checks the length of a string
- * @s: string to check
+ * _strlen - will return the length of a string.
+ * @s: string to be evaluated.
  *
- * Return: int length of the string
+ * Return: length of string.
  */
 
 int _strlen(char *s)
 {
-	int length = 0;
+	int x = 0;
 
-	while (s[length])
-		length++;
-	return (length);
+	while (*s != '\0')
+	{
+		s++;
+		x++;
+	}
+	return (x);
 }
