efba526f83865a53e78afe50fe1f7c1429715319
Londiwe <londiwek1@outlook.com>
2023-04-04 01:22:16 -0700

    insert a node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,21 +1,43 @@
 #include "lists.h"
 
 /**
- * sum_listint - will calculate the sum of all data in listint_t
- * @head: The first node in the listint_t list
+ * insert_nodeint_at_index - Will insert a new node at a given position.
+ * @head: The pointer to 1st node in listint_t
+ * @idx: The index where new node is added
+ * @n: The data to insert the new node in
  *
- * Return: The sum
+ * Return: The pointer to the new node, otherwise NULL
  */
 
-int sum_listint(listint_t *head)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	int tot = 0;
-	listint_t *temp = head;
+	listint_t *new;
+	listint_t *temp = *head;
+	unsigned int x;
 
-	while (temp)
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
 	{
-		tot += temp->n;
-		temp = temp->next;
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	return (tot);
+	for (x = 0; x < idx && temp; x++)
+	{
+		if (x == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+	return (NULL);
 }
